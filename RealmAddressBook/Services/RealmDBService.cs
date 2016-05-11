4ac39d1b4116fa5d2f3a3326ab0fe374e117c6d9
Ben Bishop <ben@rendr.io>
2016-05-11 07:48:01 -0500

    Stubbed RealmDBService and updated ViewModelLocator to include instances of our VMs.

---
+++ RealmAddressBook/Services/RealmDBService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using RealmAddressBook.Models;
+
+namespace RealmAddressBook.Services
+{
+    public class RealmDBService:IDBService
+    {
+        public RealmDBService()
+        {
+        }
+
+        #region IDBService implementation
+
+        public bool SavePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
+
--- RealmAddressBook/ViewModels/ViewModelLocator.cs
+++ RealmAddressBook/ViewModels/ViewModelLocator.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using RealmAddressBook.Services;
 
 namespace RealmAddressBook.ViewModels
 {
     public static class ViewModelLocator
     {
+
+        public static PeopleListViewModel PeopleListVM{ get; set; }
+
+        public static AddEditPersonViewModel AddEditPersonVM{ get; set; }
+
         static ViewModelLocator()
         {
+            var dbServiceToUse = new RealmDBService();
+            PeopleListVM = new PeopleListViewModel(dbServiceToUse);
+            AddEditPersonVM = new AddEditPersonViewModel(dbServiceToUse);
         }
     }
 }
